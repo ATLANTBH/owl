acf1edbb41f4ea92ce61964032b3189b04450b95
Kenan Klisura <kenan@atlantbh.com>
2017-03-16 22:39:28 +0100

    Add support for tracking time on suites

--- src/main/react/components/DurationFormat/DurationFormat.js
+++ src/main/react/components/DurationFormat/DurationFormat.js
@@ -7,24 +7,7 @@ class DurationFormat extends React.Component {
   };
 
   render() {
-    const duration = this.props.duration;
-
-    let durationString = round(duration) + 's';
-
-    if (duration < 1) {
-      durationString = Math.floor(duration * 1000) + 'ms';
-    } else {
-      if (duration > 60) {
-        let minutes = duration / 60;
-        if (minutes > 60) {
-          let hours = minutes / 60;
-          durationString = Math.floor(hours) + 'h ' + Math.floor(minutes % 60) + 'm ' + Math.floor(duration % 60);
-        } else {
-          durationString = Math.floor(minutes) + 'm ' + Math.floor(duration % 60) + 's';
-        }
-      }
-    }
-
+    const durationString = durationFormat(this.props.duration);
     return (
       <span>{durationString}</span>
     );
@@ -33,6 +16,26 @@ class DurationFormat extends React.Component {
 
 function round(value) {
   return Math.floor(value * 100) / 100;
+}
+
+export function durationFormat(duration) {
+  let durationString = round(duration) + 's';
+
+  if (duration < 1) {
+    durationString = Math.floor(duration * 1000) + 'ms';
+  } else {
+    if (duration > 60) {
+      let minutes = duration / 60;
+      if (minutes > 60) {
+        let hours = minutes / 60;
+        durationString = Math.floor(hours) + 'h ' + Math.floor(minutes % 60) + 'm ' + Math.floor(duration % 60);
+      } else {
+        durationString = Math.floor(minutes) + 'm ' + Math.floor(duration % 60) + 's';
+      }
+    }
+  }
+
+  return durationString;
 }
 
 export default DurationFormat;
