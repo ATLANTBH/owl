358f18ca475ab128f46817b041e7bc1a71e1cd6b
Kenan Klisura <kenan@atlantbh.com>
2017-05-30 10:56:17 +0200

    Revert "Remove test suites page"
    This reverts commit 193828d76b7a1b5357e45741506ee3c59223b7fc.

--- src/main/react/src/main.js
+++ src/main/react/src/main.js
@@ -8,6 +8,7 @@ import { Router, Route, browserHistory } from 'react-router';
 
 import DashboardPage from './dashboard';
 import ErrorPage from './error/index.js';
+import TestSuites from './test-suites';
 import TestRuns from './test-runs';
 import TestCases from './test-cases';
 import TestSteps from './test-steps';
@@ -29,6 +30,7 @@ function renderApplication(bootstrap) {
     <Router history={browserHistory}>
       <Route path="/" component={DashboardPage}/>
       <Route path="/test-runs" component={TestRuns}/>
+      <Route path="/test-suites" component={TestSuites}/>
       <Route path="/test-runs/:testRunId/test-cases" component={TestCases}/>
       <Route path="/test-runs/:testRunId/test-steps/*" component={TestSteps}/>
       <Route path="*" component={ErrorPage}/>
