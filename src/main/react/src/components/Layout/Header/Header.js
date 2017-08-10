9c6a061bab229e0a4b3bad9370a28003b34e2fec
Almin Islamovic <almin.islamovic@atlantbh.com>
2017-08-10 14:20:41 +0200

    Adding Logo and small nabar changes

--- src/main/react/src/components/Layout/Header/Header.js
+++ src/main/react/src/components/Layout/Header/Header.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import {Link} from 'react-router';
+import logo from '../images/Logo.png';
+import style from '../style.css';
 
 function Header() {
   // TODO(kklisura): Change Test Suites link.
   return (
     <nav className="navbar navbar-default navbar-fixed-top">
       <div className="navbar-header">
+        <Link to="/"><img src={logo} className={style.logo} /></Link>
+        
         <button type="button"
                 className="navbar-toggle collapsed"
                 data-toggle="collapse"
@@ -17,7 +21,7 @@ function Header() {
           <span className="icon-bar"></span>
           <span className="icon-bar"></span>
         </button>
-          <Link to="/" className="navbar-brand">{bootstrap.projectName}</Link>
+        <Link to="/" className="navbar-brand">{bootstrap.projectName}</Link>
       </div>
       <div id="navbar" className="navbar-collapse collapse">
         <ul className="nav navbar-nav navbar-left">
